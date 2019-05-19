213c0fb463aa53732c95114b1acf00bd4fe31030
Stuart Longland <me@vk4msl.id.au>
2019-05-19 19:44:57 +1000

    mkdeb.sh: Support Docker builds

--- mkdeb.sh
+++ mkdeb.sh
@@ -4,22 +4,16 @@
 
 set -e
 
-# Begin by ensuring we have up-to-date sources
-
-if [ -z "${SKIP_FETCH}" ]; then
-	make fetch
-fi
-
 # Set some directory locations
 : ${SRCDIR:=$( realpath $( dirname "$0" ))}
 : ${WORKDIR:=${SRCDIR}/work}
 : ${DISTDIR:=${SRCDIR}/dist}
 
-[ ! -d ${WORKDIR} ] || rm -fr ${WORKDIR}
-mkdir ${WORKDIR}
+# Begin by ensuring we have up-to-date sources
 
-[ ! -d ${DISTDIR} ] || rm -fr ${DISTDIR}
-mkdir ${DISTDIR}
+if [ -z "${SKIP_FETCH}" ]; then
+	make -C ${SRCDIR} fetch
+fi
 
 # Take the version from the mtime of the file
 : ${VERSION:=$( date -r lib_mysqludf_sys.c +%Y%m%d )}
@@ -28,18 +22,106 @@ mkdir ${DISTDIR}
 : ${DEBVER:=1}
 
 # Maintainer information
-: ${MAINTAINER:=$( sed -ne '/^Maintainer:/ { s/^Maintainer: //; p; }' \
-	${SRCDIR}/debian/control )}
+if [ -z "${MAINTAINER}" ]; then
+	MAINTAINER="$(
+		sed -ne '/^Maintainer:/ { s/^Maintainer: //; p; }' \
+		${SRCDIR}/debian/control
+	)"
+fi
 
 # Build flags
 : ${BUILD_FLAGS:=-us -uc}
 
+# Parse command-line arguments
+CLEAN_DIST=y
+CLEAN_WORK=y
+APPEND_RELEASE=y
+DOCKER=n
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--work)
+			WORKDIR="$2"
+			shift
+			;;
+		--dist)
+			DISTDIR="$2"
+			shift
+			;;
+		--debver)
+			DEBVER="$2"
+			shift
+			;;
+		--docker)
+			DOCKER=y
+			;;
+		--no-append-release)
+			APPEND_RELEASE=n
+			;;
+		--no-clean)
+			case $2 in
+				work)
+					CLEAN_WORK=n
+					;;
+				dist)
+					CLEAN_DIST=n
+					;;
+				*)
+					echo "Unknown area $2"
+					exit 1
+					;;
+			esac
+			shift
+			;;
+	esac
+	shift
+done
+
+if [ ${CLEAN_WORK} = y ]; then
+	# Create a work directory
+	[ ! -d ${WORKDIR} ] || rm -fr ${WORKDIR}
+	mkdir ${WORKDIR}
+fi
+
+if [ ${CLEAN_DIST} = y ]; then
+	# Re-create the distribution directory
+	[ ! -d ${DISTDIR} ] || rm -fr ${DISTDIR}
+	mkdir ${DISTDIR}
+fi
+
+# Are we using `docker` for this?
+if [ "${DOCKER}" = y ]; then
+	exec docker run --rm \
+		-v ${SRCDIR}:/tmp/src \
+		-v ${DISTDIR}:/tmp/dist \
+		-e MAINTAINER="${MAINTAINER}" \
+		-e VERSION=${VERSION} \
+		-e DEBVER=${DEBVER} \
+		-e BUILD_FLAGS="${BUILD_FLAGS}" \
+		-e WORKDIR=/tmp/work \
+		-e DISTDIR=/tmp/dist \
+		-e SRCDIR=/tmp/src \
+		${DOCKER_IMAGE:-sjlongland/debian-pkg-build-env} \
+		/usr/sbin/gosu $( id -u ):$( id -g ) \
+		/bin/sh -xe /tmp/src/mkdeb.sh --no-clean dist
+fi
+
 # Package name and version
 PACKAGE_VER=lib-mysqludf-sys-${VERSION}
 
+if [ ${APPEND_RELEASE} = y ]; then
+	# Append the Debian release information
+	DEBVER=${DEBVER}$( \
+		lsb_release -si | tr A-Z a-z \
+	)$( \
+		lsb_release -sr | tr . p \
+	)
+fi
+
 # Create the package source directory
 mkdir ${WORKDIR}/${PACKAGE_VER}
-cp lib_mysqludf_sys.c Makefile ${WORKDIR}/${PACKAGE_VER}
+cp ${SRCDIR}/lib_mysqludf_sys.c ${SRCDIR}/Makefile \
+	${WORKDIR}/${PACKAGE_VER}
 
 # Create the "original" tarball
 tar -C ${WORKDIR} \
